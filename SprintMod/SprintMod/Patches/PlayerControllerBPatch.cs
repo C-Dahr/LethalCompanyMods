0aaa03e64a50296d254bee33d78de0da58044d7a
C-Dahr <cameronsdahr@gmail.com>
2023-11-25 06:54:27 -0400

    Added a simple sprint mod following a tutorial

---
+++ SprintMod/SprintMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,28 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SprintMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void infiniteSprintPatch(ref float ___sprintMeter, ref float ___sprintMultiplier, ref bool ___isSprinting)
+        {
+            ___sprintMeter = 1f;
+            if (___isSprinting)
+            {
+                ___sprintMultiplier = Mathf.Lerp(___sprintMultiplier, 10f, Time.deltaTime * 1f);
+            }
+        }
+
+    }
+}
---
+++ SprintMod/SprintMod/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using SprintMod.Patches;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprintMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SprintModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Glocean.SprintMod";
+        private const string modName = "SprintMod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static SprintModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance = null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Test mod has awoken :)");
+
+            harmony.PatchAll(typeof(SprintModBase));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+
+    }
+}
